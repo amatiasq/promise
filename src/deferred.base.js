6a0ab08fdd55793c1ceaa27076c55eb6d76bccb6
A. Matías Quezada <amatiasq@gmail.com>
2013-04-09 19:31:19 +0200

    Added many test, very basic functionallity

--- src/deferred.base.js
+++ src/deferred.base.js
@@ -1,13 +1,36 @@
 'use strict';
 
 var prom = {
-	then: function() {
+	status: 'unfulfilled',
+	_cbk: { resolve: [], reject: [] },
+
+	then: function(callback, errcall) {
+		if (typeof callback === 'function')
+			this._cbk.resolve.push(callback);
+		if (typeof errcall === 'function')
+			this._cbk.reject.push(errcall);
 		return Object.create(prom);
-	}
+	},
 };
 
 function deferred() {
-	return { promise: Object.create(prom) };
+	return {
+		promise: Object.create(prom),
+
+		resolve: function(value) {
+			this.promise.status = 'fulfilled';
+			this.promise._cbk.resolve.forEach(function(a) { a(value) });
+			this.promise.then = function(callback) { callback(value); };
+			this.resolve = function() { };
+		},
+
+		reject: function(reason) {
+			this.promise.status = 'failed';
+			this.promise._cbk.reject.forEach(function(a) { a(reason) });
+			this.promise.then = function(callback, errcall) { errcall(reason) };
+			this.reject = function() { };
+		}
+	};
 }
 
 function isPromise(value) {
--- test/deferred.base.spec.js
+++ test/deferred.base.spec.js
@@ -1,6 +1,7 @@
+//jshint maxlen:200
 'use strict';
 
-//var sinon = require('sinon');
+var sinon = require('sinon');
 var assert = require('assert');
 var deferred = require('../src/deferred.base');
 
@@ -13,33 +14,135 @@ describe('Deferred', function() {
 		assert.ok(deferred());
 	});
 
-	var sut;
-	beforeEach(function() {
-		sut = deferred();
-	});
-
-	describe('#promise property', function() {
-
-		var prom;
+	describe('object', function() {
+		var sut;
 		beforeEach(function() {
-			prom = sut.promise;
+			sut = deferred();
 		});
 
-		it('should exist', function() {
-			assert.ok('promise' in sut);
+		describe('#promise property', function() {
+			var prom;
+			beforeEach(function() {
+				prom = sut.promise;
+			});
+
+			it('should exist', function() {
+				assert.ok('promise' in sut);
+			});
+
+			it('should have "unfulfilled" as default status', function() {
+				assert.equal(prom.status, 'unfulfilled');
+			});
+
+			it('should return true when passed to deferred.isPromise()', function(){
+				assert.ok(deferred.isPromise(prom));
+			});
+			it('should have a "#then()" method', function() {
+				assert.equal(typeof prom.then, 'function');
+			});
+
+			describe('#then method', function() {
+				it('should return another promise', function() {
+					assert.ok(deferred.isPromise(prom.then()));
+					assert.notEqual(prom.then(), prom);
+				});
+			});
 		});
 
-		it('should return true when passed to deferred.isPromise()', function(){
-			assert.ok(deferred.isPromise(prom));
-		});
-		it('should have a "#then()" method', function() {
-			assert.equal(typeof prom.then, 'function');
+		describe('#resolve method', function() {
+			it('should be a function', function() {
+				assert.equal(typeof sut.resolve, 'function');
+			});
+
+			var clock, spy;
+			beforeEach(function() {
+				clock = sinon.useFakeTimers();
+				spy = sinon.spy();
+			});
+
+			afterEach(function() {
+				clock.restore();
+			});
+
+			it('should change promise status to "fulfilled"', function() {
+				sut.resolve();
+				assert.equal(sut.promise.status, 'fulfilled');
+			});
+
+			it('should invoke all functions first argument passed to #promise.then() when called on the next event loop', function() {
+				sut.promise.then(spy);
+				sut.resolve();
+				clock.tick(1);
+				assert.ok(spy.calledOnce);
+			});
+
+			it('should pass it\'s argument to every callback', function() {
+				var arg = {};
+				sut.promise.then(spy);
+				sut.resolve(arg);
+				clock.tick(1);
+				assert.ok(spy.calledWithExactly(arg));
+			});
+
+			it('should do it even if #promise.then() is invoked after #resolve', function() {
+				var arg = {};
+				sut.resolve(arg);
+				sut.promise.then(spy);
+				clock.tick(1);
+				assert.ok(spy.calledWithExactly(arg));
+			});
+
+			it('should be idempotent', function() {
+				sut.promise.then(spy);
+				sut.resolve();
+				clock.tick(1);
+				sut.resolve();
+				clock.tick(1);
+				assert.ok(spy.calledOnce);
+			});
 		});
 
-		describe('#then method', function() {
-			it('should return another promise', function() {
-				assert.ok(deferred.isPromise(prom.then()));
-				assert.notEqual(prom.then(), prom);
+		describe('#reject method', function() {
+			it('should be a function', function() {
+				assert.equal(typeof sut.reject, 'function');
+			});
+
+			var clock, spy;
+			beforeEach(function() {
+				clock = sinon.useFakeTimers();
+				spy = sinon.spy();
+			});
+
+			afterEach(function() {
+				clock.restore();
+			});
+
+			it('should change promise status to "failed"', function() {
+				sut.reject();
+				assert.equal(sut.promise.status, 'failed');
+			});
+
+			it('should invoke all functions second arguments passed to #promise.then() when called on the next event loop', function() {
+				sut.promise.then(null, spy);
+				sut.reject();
+				clock.tick(1);
+				assert.ok(spy.calledOnce);
+			});
+
+			it('should pass it\'s argument to every callback', function() {
+				var arg = {};
+				sut.promise.then(null, spy);
+				sut.reject(arg);
+				clock.tick(1);
+				assert.ok(spy.calledWithExactly(arg));
+			});
+
+			it('should do it even if #promise.then() is invoked after #reject', function() {
+				var arg = {};
+				sut.reject(arg);
+				sut.promise.then(null, spy);
+				clock.tick(1);
+				assert.ok(spy.calledWithExactly(arg));
 			});
 		});
 	});
